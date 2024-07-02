ec4f56d09feb83e1a688388b99e40f6e1ed52a2e
Eric Joshua Nyato <ericnyato2@gmail.com>
2024-07-03 05:43:40 +0900

    Fixed the Chakra Menus differently from Mosh

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -22,7 +22,7 @@ const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
 
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+      <MenuButton marginLeft={4} as={Button} rightIcon={<BsChevronDown />}>
         Order by: {currentSortOrder?.label || "Relevance"}
       </MenuButton>
       <MenuList>
